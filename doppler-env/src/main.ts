1ebb39672b67e9134d750b3cdcd23269b3af8fbc
Dominic Sherman <dominic.sherman98@gmail.com>
2022-04-11 15:14:27 -0700

    Add info logging

--- doppler-env/src/main.ts
+++ doppler-env/src/main.ts
@@ -4,13 +4,20 @@ import * as core from '@actions/core';
 const main = async (): Promise<void> => {
   const releaseStage = core.getInput('releaseStage');
   const tokenKey = `${releaseStage.toUpperCase()}_DOPPLER_TOKEN`;
+
+  core.info(`Pulling token from ${tokenKey}`);
+
   const token = process.env[tokenKey];
 
   if (!token) {
     throw new Error(`No token for ${tokenKey} in environment`);
+  } else {
+    core.info('Token found');
   }
 
   await exec(`doppler secrets download --no-file --format=env --token=${token} > .env`);
+
+  core.info('Secrets downloaded');
 };
 
 main().catch((e) => core.setFailed(e instanceof Error ? e.message : JSON.stringify(e)));
