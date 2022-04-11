9a9dae4605bba265d906c88612fe498a668683a8
Dominic Sherman <dominic.sherman98@gmail.com>
2022-04-11 15:23:21 -0700

    Move dependent step into callback

--- doppler-env/src/main.ts
+++ doppler-env/src/main.ts
@@ -15,23 +15,24 @@ const main = async (): Promise<void> => {
     core.info('Token found');
   }
 
-  await exec(
+  exec(
     '(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh',
     (err) => {
       if (err) {
         core.setFailed(err instanceof Error ? err.message : JSON.stringify(err));
       } else {
         core.info('Doppler CLI installed');
+
+        exec(`doppler secrets download --no-file --format=env --token=${token} > .env`, (err) => {
+          if (err) {
+            core.setFailed(err instanceof Error ? err.message : JSON.stringify(err));
+          } else {
+            core.info('Secrets downloaded');
+          }
+        });
       }
     }
   );
-  await exec(`doppler secrets download --no-file --format=env --token=${token} > .env`, (err) => {
-    if (err) {
-      core.setFailed(err instanceof Error ? err.message : JSON.stringify(err));
-    } else {
-      core.info('Secrets downloaded');
-    }
-  });
 };
 
 main().catch((e) => core.setFailed(e instanceof Error ? e.message : JSON.stringify(e)));
